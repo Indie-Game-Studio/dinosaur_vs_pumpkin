39c48581ef27172e31e87ff0f7ab97afb56393af
kerlw <kerl.d.w@gmail.com>
2019-10-28 22:06:42 +0800

    changed camera controller to target follow camera use arrow key to control pumpkin moving

--- Assets/Scripts/Pumpkin.cs
+++ Assets/Scripts/Pumpkin.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class Pumpkin : MonoBehaviour {
-    
+    public float speed = 9;
+
+    public float rotateSpeed = 60;
     
     // Start is called before the first frame update
     void Start() {
@@ -11,5 +14,14 @@ public class Pumpkin : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            transform.Rotate (Vector3.up * -rotateSpeed * Time.deltaTime);
+        } else if (Input.GetKey(KeyCode.RightArrow)) {
+            transform.Rotate (Vector3.up * rotateSpeed * Time.deltaTime);
+        }
+        
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        } 
     }
 }
