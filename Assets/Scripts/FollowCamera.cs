ca76b19d1957e12249c2c1fd1b14c368b0c12783
kerlw <kerl.d.w@gmail.com>
2019-10-29 20:12:15 +0800

    add follow camera

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+    public Transform target;
+
+    public float smoothSpeed = 10f;
+
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+        var destPosition = target.position + offset;
+        var smoothedPosition = Vector3.Lerp(transform.position, destPosition, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
+    }
+}
