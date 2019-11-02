d8a92a32e79345e945991ce9df40568b73112184
ispostback <ispostback@gmail.com>
2019-11-02 21:09:22 +0800

    1

---
+++ Assets/Game/Scripts/Objects/Pumpkin.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pumpkin : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public float turnSmoothing = 15f;
+
+    Rigidbody m_rigid;
+    Animator m_animator;
+    
+    void Awake()
+    {
+        m_rigid = GetComponent<Rigidbody>();
+        m_animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        if (h != 0f || v != 0f)
+        {
+            Moving(h, v);
+            Rotating(h, v);
+            float moveSpeed = Mathf.Abs(h * h + v * v);
+            m_animator.SetFloat("moveSpeed", moveSpeed);
+        }
+        else {
+            m_animator.SetFloat("moveSpeed", 0f);
+        }
+    }
+
+    void Rotating(float horizontal, float vertical)
+    {
+        Vector3 targetDirection = new Vector3(horizontal, 0f, vertical);
+        Quaternion targetRotation = Quaternion.LookRotation(targetDirection, Vector3.up);
+        Quaternion newRotation = Quaternion.Lerp(m_rigid.rotation, targetRotation, turnSmoothing * Time.deltaTime);
+        m_rigid.MoveRotation(newRotation);
+    }
+    void Moving(float horizontal, float vertical) {
+
+        Vector3 deltaPos = new Vector3(horizontal, 0f, vertical);
+        Vector3 newPosition = m_rigid.position + deltaPos * moveSpeed * Time.deltaTime;
+        m_rigid.MovePosition(newPosition);
+    }
+}
