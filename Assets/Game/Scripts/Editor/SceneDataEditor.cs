fd077492f35d45e628c8f0d687f25b9918832d90
kerl.d.w <kerl.d.w@gmail.com>
2019-11-18 14:31:37 +0800

    SceneData的inspector加入新建南瓜怪出生点的功能

--- Assets/Game/Scripts/Editor/SceneDataEditor.cs
+++ Assets/Game/Scripts/Editor/SceneDataEditor.cs
@@ -1,10 +1,23 @@
+using System;
 using UnityEngine;
 using UnityEditor;
 using UnityEditor.SceneManagement;
 
 [CustomEditor(typeof(SceneData))]
 public class SceneDataEditor: Editor {
-    
+    private SerializedProperty propertyPumpkinSpawn;
+
+    private bool showPropertyPumpkinSpawn = true;
+
+    private const string propertyPumpkinSpawnName = "pumpkinSpawnPos";
+
+    private SceneData sceneData;
+
+    private void OnEnable() {
+        sceneData = (SceneData) target;
+        propertyPumpkinSpawn = serializedObject.FindProperty(propertyPumpkinSpawnName);
+    }
+
     // GameObject的menu中的priority低于50的item同样会出现在Hierarchy的Context Menu中
     [MenuItem("GameObject/Create SceneData", priority = 49)]
     public static void CreateSceneDataObject() {
@@ -16,6 +29,32 @@ public class SceneDataEditor: Editor {
     }
     
     public override void OnInspectorGUI() {
-        base.OnInspectorGUI();
+        serializedObject.Update();
+        
+        EditorGUILayout.LabelField("场景数据");
+        EditorGUILayout.Space();
+
+        showPropertyPumpkinSpawn = EditorGUILayout.Foldout(showPropertyPumpkinSpawn, "南瓜怪出生点");
+        if (showPropertyPumpkinSpawn) {
+            EditorGUI.indentLevel++;
+            if (sceneData.pumpkinSpawnPos == null) {
+                if (GUILayout.Button("创建 南瓜怪出生点")) {
+                    sceneData.pumpkinSpawnPos = CreatePumpkinSpawn().transform;
+                    Repaint();
+                }
+            } else {
+                EditorGUILayout.PropertyField(propertyPumpkinSpawn);
+            }
+            EditorGUI.indentLevel--;
+        }
+        EditorGUILayout.Space();
+    }
+
+    private GameObject CreatePumpkinSpawn() {
+        GameObject obj = new GameObject("pumpkin spawn");
+        obj.AddComponent<PumpkinSpawnPos>();
+        obj.transform.parent = ((SceneData) target).transform;
+        Undo.RegisterCreatedObjectUndo(obj, "Pumpkin Spawn Object");
+        return obj;
     }
 }
